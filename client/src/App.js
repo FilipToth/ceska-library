46e3c282ab8647f37a982fcb1b0d9630e1271bae
FilipToth <filip.toth@accelley.com>
2022-09-17 03:59:22 -0400

    Add search suggestions

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
+import Search from './Search'
+import SearchResults from './SearchResults';
+import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom' 
+
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    
+    return (
+        <Router>
+            <div className='App-Container'>
+                <Switch>
+                    <Route exact path='/'>
+                        <Search />
+                    </Route>
+                    <Route path='/results'>
+                        <SearchResults />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ client/src/SearchSuggestion/SearchSuggestion.js
@@ -0,0 +1,11 @@
+import './SearchSuggestion.css'
+
+function SearchSuggestion() {
+    return (
+        <div className='Suggestion'>
+            <p1 className='Suggestion-Text'>Suggestion...</p1>
+        </div>
+    )
+}
+
+export default SearchSuggestion;
