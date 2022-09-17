3a6ee45e36c07543d4d857965e685346bfc2fa32
FilipToth <filip.toth@accelley.com>
2022-09-17 03:58:55 -0400

    Add search results page

---
+++ client/src/SearchResults/SearchResults.js
@@ -0,0 +1,21 @@
+import './SearchResults.css'
+
+/*
+const SearchResults = () => {
+    return (
+        <div>
+            <p1>#FuckTheCustomer!</p1>
+        </div>
+    )
+};
+*/
+
+function SearchResults() {
+    return (
+        <div>
+            <p1>FuckTheCustomer!</p1>
+        </div>
+    )
+}
+
+export default SearchResults;
