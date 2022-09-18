6826b55c8a295fd15a29c845146930a749a0cd11
FilipToth <filip.toth@accelley.com>
2022-09-18 04:14:15 -0400

    Add proper content editable

--- client/src/SearchBar/SearchBar.js
+++ client/src/SearchBar/SearchBar.js
@@ -1,11 +1,17 @@
 import './SearchBar.css'
 import SearchButton from '../SearchButton';
 import SearchSuggestion from '../SearchSuggestion';
+import ContentEditable from 'react-contenteditable'
+import { useState } from 'react';
 
 const SearchBar = (props) => {
+
+    const searchPlaceholder = "Search Books..."
+    const [search, setSearch] = useState({ html: `${searchPlaceholder}`});
+
     let searchQuery = props.searchQuery;
     if (searchQuery == undefined)
-        searchQuery = "Search Books...";
+        searchQuery = searchPlaceholder;
     
     let suggestionsJSX;
     const renderSuggestions = false;
@@ -20,16 +26,25 @@ const SearchBar = (props) => {
             </div>
         )
     }
+
+    const searchFieldSelected = () => {
+        if (search.html == searchPlaceholder)
+            setSearch({ html: '' });
+    };
+
+    const searchFieldChanged = (ev) => {
+        setSearch({ html: ev.target.value } );
+    }
     
     return (
         <div className='Search-Container'>
             <div className='Suggest-Book-Div-Button'>
-                <p1 className='Suggest-Book-Text'>Suggest me a book!</p1>
+                <p1 className='Suggest-Book-Text'>Suggest me a book!</p1>-
             </div>
             
             <div className='Lower-Search-Container'>
                 <div className='Search-Div'>
-                    <p1 className='Search-Books-Text'>{searchQuery}</p1>
+                    <ContentEditable html={search.html} onChange={searchFieldChanged} className='Search-Books-Text' onSelect={searchFieldSelected} />
                     <SearchButton />
                 </div>
             </div>
